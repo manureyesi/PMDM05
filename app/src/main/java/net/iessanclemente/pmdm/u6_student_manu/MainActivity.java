74f77751d9e38457d7d4f9d4b59dc90ef610cb12
manureyesi <manureyesi@outlook.es>
2021-04-28 18:32:56 +0200

    Init app

---
+++ app/src/main/java/net/iessanclemente/pmdm/u6_student_manu/MainActivity.java
@@ -0,0 +1,13 @@
+package net.iessanclemente.pmdm.u6_student_manu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
