a2d6ac9de1911aa7e278c303d00d484881bb6466
manureyesi <manureyesi@outlook.es>
2021-05-04 19:26:36 +0200

    Crear una nueva activity

---
+++ app/src/main/java/net/iessanclemente/pmdm/u6_student_manu/U6StudentManuActivity2.java
@@ -0,0 +1,13 @@
+package net.iessanclemente.pmdm.u6_student_manu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class U6StudentManuActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_u6_student_manu2);
+    }
+}
