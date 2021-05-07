00c3934b94c0c47b6640ede22af5bb56b9c2ddd8
manureyesi <manureyesi@outlook.es>
2021-05-08 01:06:16 +0200

    Añadir método para string provincias

--- app/src/main/java/net/iessanclemente/pmdm/u6_student_manu/U6StudentManuActivity2.java
+++ app/src/main/java/net/iessanclemente/pmdm/u6_student_manu/U6StudentManuActivity2.java
@@ -41,13 +41,33 @@ public class U6StudentManuActivity2 extends AppCompatActivity {
             Log.i(TAG, "Provincia - ".concat(this.textoProvincia.getText().toString()));
 
             Intent datos_vuelta = new Intent();
-            datos_vuelta.putExtra(U6StudentManuMain.PROVINCIA, this.textoProvincia.getText().toString());
+            datos_vuelta.putExtra(U6StudentManuMain.PROVINCIA, toUpperCaseFirstChar(this.textoProvincia.getText().toString()));
             setResult(RESULT_OK, datos_vuelta);
         }
 
         // Finalizamos la activity secundaria
         super.finish();
 
+    }
+
+    /**
+     * Poner en mayusculas la primera letra de la cadena
+     * @param cadena
+     * @return
+     */
+    private String toUpperCaseFirstChar (final String cadena) {
+
+        String cadenaStr = null;
+        if (cadena != null && cadena.length() == 1) {
+            cadenaStr = cadena.toUpperCase();
+        } else if (cadena.length() > 1) {
+            StringBuilder cadenaFinal = new StringBuilder();
+            cadenaFinal.append(Character.toString(cadena.charAt(0)).toUpperCase());
+            cadenaFinal.append(cadena.substring(1));
+            cadenaStr = cadenaFinal.toString();
+        }
+
+        return cadenaStr;
     }
 
 }
