4ff9991cbb079f7fa64aa43bf96135fdcf75278e
manureyesi <manureyesi@outlook.es>
2021-05-08 01:06:52 +0200

    Limpiar imports

--- app/src/main/java/net/iessanclemente/pmdm/u6_student_manu/U6StudentManuMain.java
+++ app/src/main/java/net/iessanclemente/pmdm/u6_student_manu/U6StudentManuMain.java
@@ -2,9 +2,7 @@ package net.iessanclemente.pmdm.u6_student_manu;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
